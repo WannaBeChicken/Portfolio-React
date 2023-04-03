e8498505a45a7196572cbed621a4f0a5d78458d9
WannaBeChicken <prathamdavbistupur@gmail.com>
2023-04-04 01:05:43 +0530

    Scroll transition is now working

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,10 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { HashLink as Link} from 'react-router-hash-link';
-
+import { HashLink as Link } from "react-router-hash-link";
+import ScrollToHashElement from "./ScrollToHashElement";
+// import Header from "./Header";
+// import Content from "./Content";
 import {
   AiOutlineAudit,
   AiOutlineHome,
@@ -13,8 +15,8 @@ import {
 } from "react-icons/ai";
 
 function NavBar() {
-  const [ issticky ,updateNavbar] = React.useState(false);
-  function checkIfSticky(){
+  const [issticky, updateNavbar] = React.useState(false);
+  function checkIfSticky() {
     if (window.scrollY >= 20) {
       updateNavbar(true);
     } else {
@@ -22,22 +24,26 @@ function NavBar() {
     }
   }
   window.addEventListener("scroll", checkIfSticky);
-
+  
   return (
+    
     <Container className="navbar-container">
+      <ScrollToHashElement />
+      {/* <Header /> */}
+      {/* <Content /> */}
       <Navbar className={issticky && "sticky"} bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Pratham</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="#home">
+            <Nav.Link as={Link} smooth to="#home">
               <AiOutlineHome className="icon" />
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="#about">
+            <Nav.Link as={Link} smooth to="#about">
               <AiOutlineUser className="icon" />
               About
             </Nav.Link>
-            <Nav.Link as={Link} to="/project" >
+            <Nav.Link as={Link} to="/project">
               <AiOutlineFundProjectionScreen className="icon" />
               Project
             </Nav.Link>
